4e5470bfd3db5ea39b3cd89fd67e8478f6322494
suzzy05 <shilpishaw0933@gmail.com>
2026-03-23 18:09:32 +0530

    Add Token.java to scanner package to represent tokens generated by scanner.

--- src/scanner/Token.java
+++ src/scanner/Token.java
@@ -1,1 +1,25 @@
+package scanner;
 
+public class Token {
+    private final TokenType type;
+    private final String value;
+    private final int line;
+
+    public Token(TokenType type, String value, int line) {
+        this.type = type;
+        this.value = value;
+        this.line = line;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getLine() {
+        return line;
+    }
+}
