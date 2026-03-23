1fdea9bd21b220d96b88d7e0a7a3cdb75e92b82a
suzzy05 <shilpishaw0933@gmail.com>
2026-03-23 18:00:33 +0530

    Add TokenType.java to scanner package to define token categories used by scanner.

--- src/scanner/TokenType.java
+++ src/scanner/TokenType.java
@@ -1,1 +1,25 @@
+package scanner;
 
+public enum TokenType {
+    NUMBER,
+    STRING,
+    IDENTIFIER,
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    ASSIGN,      // :=
+    PRINT,       // >>
+    IF,          // ?
+    REPEAT,      // @
+    ARROW,       // =>
+    GT,
+    LT,
+    EQEQ,
+    LPAREN,
+    RPAREN,
+    NEWLINE,
+    INDENT,
+    DEDENT,
+    EOF
+}
