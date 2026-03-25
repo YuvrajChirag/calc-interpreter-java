e28819fc3d76bf16a495ba9b284390a98132b4ab
Mausam Kumari <jnvmousam777@gmail.com>
2026-03-25 22:35:21 +0530

    Add NumberNode for numeric expressions

--- src/parser/NumberNode.java
+++ src/parser/NumberNode.java
@@ -1,1 +1,16 @@
+package parser;
 
+import environment.Environment;
+
+public class NumberNode implements Expression {
+    private final double value;
+
+    public NumberNode(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public Object evaluate(Environment env) {
+        return value;
+    }
+}
