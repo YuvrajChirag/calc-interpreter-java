3f15cf08963823c184373e5dec392059776962b6
Mausam Kumari <jnvmousam777@gmail.com>
2026-03-27 23:20:02 +0530

    Add VariableNode class

--- src/parser/VariableNode.java
+++ src/parser/VariableNode.java
@@ -1,1 +1,17 @@
+package parser;
+
+import environment.Environment;
+
+public class VariableNode implements Expression {
+    private final String name;
+
+    public VariableNode(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object evaluate(Environment env) {
+        return env.get(name);
+    }
+}
 
