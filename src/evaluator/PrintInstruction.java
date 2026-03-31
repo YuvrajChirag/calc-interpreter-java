5f2640266b907023e6cddaf2c6b68c612600340e
YuvrajChirag <yuvrajchirag9se@gmail.com>
2026-03-31 10:33:56 +0530

    Add PrintInstruction class to support expression output in evaluator

--- src/evaluator/PrintInstruction.java
+++ src/evaluator/PrintInstruction.java
@@ -1,1 +1,26 @@
+package evaluator;
+
+import environment.Environment;
+import parser.Expression;
+
+public class PrintInstruction implements Instruction {
+    private final Expression expression;
+
+    public PrintInstruction(Expression expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public void execute(Environment env) {
+        Object value = expression.evaluate(env);
+        if (value instanceof Number) {
+            double d = ((Number) value).doubleValue();
+            if (d == Math.rint(d)) {
+                System.out.println((long) d);
+                return;
+            }
+        }
+        System.out.println(value);
+    }
+}
 
