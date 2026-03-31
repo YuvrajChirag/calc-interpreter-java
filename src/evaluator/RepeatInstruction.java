f4a7aefa094377247d1f27e53e16fc17dc94f024
YuvrajChirag <yuvrajchirag9se@gmail.com>
2026-03-31 17:37:50 +0530

    Add RepeatInstruction class to support loop execution in evaluator

--- src/evaluator/RepeatInstruction.java
+++ src/evaluator/RepeatInstruction.java
@@ -1,1 +1,32 @@
+package evaluator;
+
+import environment.Environment;
+import parser.Expression;
+
+import java.util.List;
+
+public class RepeatInstruction implements Instruction {
+    private final Expression countExpression;
+    private final List<Instruction> body;
+
+    public RepeatInstruction(Expression countExpression, List<Instruction> body) {
+        this.countExpression = countExpression;
+        this.body = body;
+    }
+
+    @Override
+    public void execute(Environment env) {
+        Object countValue = countExpression.evaluate(env);
+        if (!(countValue instanceof Number)) {
+            throw new RuntimeException("Repeat count must be numeric but got: " + countValue);
+        }
+
+        int count = (int) Math.floor(((Number) countValue).doubleValue());
+        for (int i = 0; i < count; i++) {
+            for (Instruction instruction : body) {
+                instruction.execute(env);
+            }
+        }
+    }
+}
 
