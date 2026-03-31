f2e8ab20b01e895c45be109ec1a2bee54f66c155
YuvrajChirag <yuvrajchirag9se@gmail.com>
2026-03-31 23:16:23 +0530

    Add Interpreter class to execute parsed instructions from source code

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -1,1 +1,25 @@
+package interpreter;
+
+import environment.Environment;
+import evaluator.Instruction;
+import parser.Parser;
+import scanner.Token;
+import scanner.Tokenizer;
+
+import java.util.List;
+
+public class Interpreter {
+    public void run(String sourceCode) {
+        Tokenizer tokenizer = new Tokenizer(sourceCode);
+        List<Token> tokens = tokenizer.tokenize();
+
+        Parser parser = new Parser(tokens);
+        List<Instruction> instructions = parser.parse();
+
+        Environment environment = new Environment();
+        for (Instruction instruction : instructions) {
+            instruction.execute(environment);
+        }
+    }
+}
 
