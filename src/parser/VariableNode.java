8086eb710d889aa90e41d1cdf492cdc1f308a66f
Mausam Kumari <jnvmousam777@gmail.com>
2026-04-13 23:58:29 +0530

    Add comments to variablenode class

--- src/parser/VariableNode.java
+++ src/parser/VariableNode.java
@@ -2,15 +2,20 @@ package parser;
 
 import environment.Environment;
 
+// VariableNode represents a variable (like x, y, etc.) in the expression
 public class VariableNode implements Expression {
+    // Stores the variable name (e.g., "x", "y")
     private final String name;
 
+    // Constructor: initializes the variable name
     public VariableNode(String name) {
         this.name = name;
     }
 
+    // Evaluate method: fetches the value of the variable from environment
     @Override
     public Object evaluate(Environment env) {
+        // Get the value associated with the variable name
         return env.get(name);
     }
 }
