27fa0c7fd9151af05a562976f0564c7e07d5087f
Mausam Kumari <jnvmousam777@gmail.com>
2026-04-13 23:48:50 +0530

    Add comments to stringnode class

--- src/parser/StringNode.java
+++ src/parser/StringNode.java
@@ -1,16 +1,21 @@
-package parser;
+package parser;// This class belongs to parser package
 
-import environment.Environment;
+import environment.Environment;// Import Environment (required for evaluate method)
 
+// StringNode represents a constant string value in the expression tree
 public class StringNode implements Expression {
+    // Stores the string value (e.g., "hello", "abc", etc.)
     private final String val;
 
+    // Constructor: initializes the string value
     public StringNode(String val) {
         this.val = val;
     }
 
+    // Evaluate method: returns the stored string
+    // No need to use environment because it's a constant value
     @Override
     public Object evaluate(Environment env) {
-        return val;
+        return val;// simply return the string
     }
 }
